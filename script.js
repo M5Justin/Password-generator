fb60987bf98bac0ce30af3eb916f3077ca5513a0
justin moran <justinmoranre@gmail.com>
2022-11-30 18:03:34 -0500

    reboot and initial prompts

--- script.js
+++ script.js
@@ -3,36 +3,68 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   //your code goes here
-  var possibleSpecial = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
-  var letters = "abcdefghijklmnopqrstuvwxyz";
-  var numbers = "0987654321";
-  const input = prompt("How many characters would you like your password to be?");
-  var isUppercase = confirm("Would you like to use uppercase?");
-  var isLowercase = confirm("Would you like to use lowercase?");
-  var isSpecial = confirm("Would you like to use a special character?");
-  var isNumber = confirm("Would you like to use a number?");
+  var input;
+  var upletters;
+  var lowercase;
+  var special;
+  var numbers;
+  var choices;
+
+  special = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+  lowercase = "abcdefghijklmnopqrstuvwxyz";
+  upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  numbers = "0987654321";
   
   
-  if (input) {
-    console.log(input)
+  //user input of password length
+  input = parseInt(prompt("How many characters would you like your password to be? It can be 8-128 characters."));
+  if (!input) {
+    alert("You need to choose a number between 8 and 128");
+  } else if (input < 8 || input > 128) {
+    input = parseInt(prompt("You need to choose a number between 8 and 128"))
+  } else {
+    upLetters = confirm("Would you like to use uppercase letters?");
+    lowercase = confirm("Would you like to use lowercase letters?");
+    special = confirm("Would you like to use special characters?");
+    numbers = confirm("Would you like to use numbers?");
   }
 
-  if (isUppercase) {
+
+
+  
+   if (isUpletters) {
     console.log("uppercase");
+  } else {
+    var characters = characters - upLetters
   }
 
   if (isLowercase) {
     console.log("lowercase");
+  } else {
+    var characters = characters - lowercase
+
   }
   
   if (isSpecial) {
     console.log("special");
+  } else {
+    var characters = characters - special
   }
 
-  if (isNumber) {
+  if (isNumbers) {
     console.log("number");
+  } else {
+    var characters = characters - numbers
   }
 
+  for (var i = 0; i <= passLength; i++) {
+    var input = passLength
+    var randomNumber = Math.floor(Math.random() * characters.length);
+    password += characters.substring(randomNumber, randomNumber + 1);
+    console.log(password)
+  }
+    document.querySelector("#generate").value = password;
+  
   // grab random letters
 
   return "";
@@ -49,3 +81,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
