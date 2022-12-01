a060695c3bc3c9a89c528991e4d6cf6547978543
justin moran <justinmoranre@gmail.com>
2022-12-01 10:09:52 -0500

    fixed comment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  //your code goes here
+  
   var Upletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var Lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var Special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
